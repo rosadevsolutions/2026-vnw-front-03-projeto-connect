e7bb08ff57f086b8498217a183a26f7d45a8f354
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-27 19:38:51 -0300

    assets images e index.js com endpoints

---
+++ src/assets/images/index.js
@@ -0,0 +1,30 @@
+import doacaoImagem1 from "./doacao-instituto-grande-familia.png";
+import doacaoImagem2 from "./doacao-projeto-futuro-escola.png";
+import doacaoImagem3 from "./doacao-instituto-conecta-jovem.png";
+
+import eventosImagem1 from "./eventos-empoderando-jovens-futuro.png";
+import eventosImagem2 from "./eventos-tecnologia-que-transforma.png";
+import eventosImagem3 from "./eventos-carreira-primeiro-emprego.png";
+
+import mentoriaImagem1 from "./mentoria-carreira-emprego.png";
+import mentoriaImagem2 from "./mentoria-compartilhe-experiencias.png";
+import mentoriaImagem3 from "./mentoria-acompanhamento.png";
+
+import voluntariadoImagem1 from "./voluntariado-mutirao-reciclagem.png";
+import voluntariadoImagem2 from "./voluntariado-aulas-tecnologia.png";
+import voluntariadoImagem3 from "./voluntariado-esporte-inclusao.png";
+
+export const images = {
+  "doacao-instituto-grande-familia.png": doacaoImagem1,
+  "doacao-projeto-futuro-escola.png": doacaoImagem2,
+  "doacao-instituto-conecta-jovem.png": doacaoImagem3,
+  "eventos-empoderando-jovens-futuro.png": eventosImagem1,
+  "eventos-tecnologia-que-transforma.png": eventosImagem2,
+  "eventos-carreira-primeiro-emprego.png": eventosImagem3,
+  "mentoria-carreira-emprego.png": mentoriaImagem1,
+  "mentoria-compartilhe-experiencias.png": mentoriaImagem2,
+  "mentoria-acompanhamento.png": mentoriaImagem3,
+  "voluntariado-mutirao-reciclagem.png": voluntariadoImagem1,
+  "voluntariado-aulas-tecnologia.png": voluntariadoImagem2,
+  "voluntariado-esporte-inclusao.png": voluntariadoImagem3,
+};
