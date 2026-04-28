2d22c4b2f084a3006db979b66e5455acd98c0403
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-28 04:39:35 -0300

    component PerfilUusario refatorado

--- src/pages/PerfilUsuario/PerfilUsuario.jsx
+++ src/pages/PerfilUsuario/PerfilUsuario.jsx
@@ -1,86 +1,93 @@
 import './PerfilUsuario.scss';
-import { Link } from "react-router-dom"
-import ImageUser  from "../../assets/images/user-thumbnail.png";
+import ImageUser from "../../assets/images/user-thumbnail.png";
 import IconCursor from "../../assets/icons/icon-cursor.svg?react";
-import IconEmail  from "../../assets/icons/icon-email.svg?react";
+import IconEmail from "../../assets/icons/icon-email.svg?react";
 import IconCalendar from "../../assets/icons/icon-calendar.svg?react";
 
+function Mailto({ email, subject = "", body = "", children }) {
+  const params =
+    subject || body
+      ? `?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
+      : "";
+
+  return <a href={`mailto:${email}${params}`}>{children}</a>;
+}
+
 export default function PerfilUsuario() {
-  const Mailto = ({ email, subject = "", body = "", children }) => {
-  const params = subject || body
-    ? `?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
-        body
-      )}`
-    : "";
-    return <a href={`mailto:${email}${params}`}>{children}</a>;
-  };
+  const infos = [
+    {
+      id: 1,
+      icon: <IconCursor />,
+      content: <p>Rio de Janeiro, RJ</p>,
+    },
+    {
+      id: 2,
+      icon: <IconEmail />,
+      content: (
+        <Mailto email="rosadevsolutions@gmail.com">
+          rosadevsolutions@gmail.com
+        </Mailto>
+      ),
+    },
+    {
+      id: 3,
+      icon: <IconCalendar />,
+      content: <p>Membro desde 01/2022</p>,
+    },
+  ];
+
+  const tags = [
+    "Front-End",
+    "VTex FastStore",
+    "VTex IO",
+    "VTex CMS",
+    "JS Stack",
+  ];
 
   return (
-    <>
-      <section className="perfilUsuario__wrapper">
-        <div className="perfilUsuario__container">
-          <div className="perfilUsuario__content">
-            <div className="perfilUsuario__image--wrapper">
-              <img
-                className="perfilUsuario__image--thumbnail"
-                src={ImageUser}
-                title="foto do usuario"
-                alt="foto do usuario"
-              />
-            </div>
-            <div className="perfilUsuario__infos--wrapper">
-              <h5 className="perfilUsuario__infos--user">
-                Romulo Rosa
-              </h5>
-              <p className="perfilUsuario__infos--position">
-                ❤️ Voluntário Ativo
-              </p>
-              <p className="perfilUsuario__infos--bio">
-                Apaixonado por fazer a diferença na comunidade. <br />Acredito que pequenas ações podem transformar vidas e estou sempre em busca de novas oportunidades para ajudar.
-              </p>
+    <section className="perfilUsuario__wrapper">
+      <div className="perfilUsuario__container">
+        <div className="perfilUsuario__content">
+          <div className="perfilUsuario__image--wrapper">
+            <img
+              className="perfilUsuario__image--thumbnail"
+              src={ImageUser}
+              alt="foto do usuario"
+            />
+          </div>
 
-              <div className="perfilUsuario__infos--icons">
-                <div className="perfilUsuario__infos--icon">
-                  <IconCursor />
-                  <p>Rio de Janeiro, RJ</p>
+          <div className="perfilUsuario__infos--wrapper">
+            <h5 className="perfilUsuario__infos--user">Romulo Rosa</h5>
+
+            <p className="perfilUsuario__infos--position">
+              ❤️ Voluntário Ativo
+            </p>
+
+            <p className="perfilUsuario__infos--bio">
+              Apaixonado por fazer a diferença na comunidade.
+              <br />
+              Acredito que pequenas ações podem transformar vidas e estou sempre em busca de novas oportunidades para ajudar.
+            </p>
+
+            <div className="perfilUsuario__infos--icons">
+              {infos.map((info) => (
+                <div className="perfilUsuario__infos--icon" key={info.id}>
+                  {info.icon}
+                  {info.content}
                 </div>
-                <div className="perfilUsuario__infos--icon">
-                  <IconCursor />
-                  <Mailto
-                    className="perfilUsuario__infos--email"
-                    email="rosadevsolutions@gmail.com"
-                    subject
-                    body
-                  >
-                    rosadevsolutions@gmail.com
-                  </Mailto>
+              ))}
+            </div>
+
+            <div className="perfilUsuario__infos--tags">
+              {tags.map((tag) => (
+                <div className="perfilUsuario__infos--tag" key={tag}>
+                  {tag}
                 </div>
-                <div className="perfilUsuario__infos--icon">
-                  <IconCalendar />
-                  <p>Membro desde 01/2022</p>
-                </div>
-              </div>
-              <div className="perfilUsuario__infos--tags">
-                <div className="perfilUsuario__infos--tag">
-                  Front-End
-                </div>
-                <div className="perfilUsuario__infos--tag">
-                  VTex FastStore
-                </div>
-                <div className="perfilUsuario__infos--tag">
-                  VTex IO
-                </div>
-                <div className="perfilUsuario__infos--tag">
-                  VTex CMS
-                </div>
-                <div className="perfilUsuario__infos--tag">
-                  JS Stack
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
-      </section>
-    </>
-  )
-};
+      </div>
+    </section>
+  );
+}
