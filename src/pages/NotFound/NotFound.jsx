545b66468c439acb6d9406758fdba158e7631a1b
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-28 05:12:38 -0300

    page NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom"
+import './NotFound.scss';
+
+
 export default function NotFound() {
-  return <h1>Not Found</h1>;
+  return (
+    <section className="notFound__wrapper">
+      <div className="notFound__container">
+        <div className="notFound__content">
+
+          <h2 className="notFound__title">
+            404
+          </h2>
+          <h3 className="notFound__subtitle">
+            Erro de Página Não Encontrada
+          </h3>
+          <Link
+            to="/"
+            aria-label="Home"
+            className="notFound__link"
+          >
+            Clique aqui e retorne para Home
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
