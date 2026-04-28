343859f5a874206a17b0af8f20d237f3c3d16928
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-27 22:49:28 -0300

    component areas refatorado

--- src/components/shared/Footer/Areas/Areas.jsx
+++ src/components/shared/Footer/Areas/Areas.jsx
@@ -1,63 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./Areas.scss";
+
+const areas = [
+  {
+    label: "Tecnologia",
+    url: "https://www.linkedin.com/in/rosadevsolutions/",
+    name: "Rosadev Solutions",
+    linkClass: "footer__link-dev",
+    logoClass: "footer__logo-dev",
+  },
+  {
+    label: "Ensino",
+    url: "https://vainaweb.com.br/",
+    name: "Vai na Web",
+    linkClass: "footer__link-ensino",
+    logoClass: "footer__logo-ensino",
+  },
+  {
+    label: "Apoio",
+    url: "https://empowerweb.org/",
+    name: "EMpower",
+    linkClass: "footer__link-apoio",
+    logoClass: "footer__logo-apoio",
+  },
+];
 
 export default function Areas() {
   return (
     <div className="footer__areas">
-      <div className="footer__area">
-        <p className="footer__text">
-          Tecnologia:
-        </p>
-        <div className="footer__link-wrapper">
-          <Link
-            to="https://www.linkedin.com/in/rosadevsolutions/"
-            className="footer__link-dev"
-            title="Rosadev Solutions"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h5 className="footer__logo-dev">
-              Rosadev Solutions
-            </h5>
-          </Link>
-        </div>
-      </div>
-      <div className="footer__area">
-        <p className="footer__text">
-          Ensino:
-        </p>
-        <div className="footer__link-wrapper">
-          <Link
-          to="https://vainaweb.com.br/"
-          className="footer__link-ensino"
-          title="Vai na Web"
-          target="_blank"
-          rel="noopener noreferrer"
+      {areas.map((area) => (
+        <div
+          className="footer__area"
+          key={area.name}
         >
-          <h5 className="footer__logo-ensino">
-            Vai na Web
-          </h5>
-        </Link>
+          <p className="footer__text">
+            {area.label}:
+          </p>
+          <div className="footer__link-wrapper">
+            <Link
+              to={area.url}
+              className={area.linkClass}
+              title={area.name}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h5 className={area.logoClass}>
+                {area.name}
+              </h5>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="footer__area">
-        <p className="footer__text">
-          Apoio:
-        </p>
-        <div className="footer__link-wrapper">
-          <Link
-          to="https://empowerweb.org/"
-          className="footer__link-apoio"
-          title="EMpower"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h5 className="footer__logo-apoio">
-            EMpower
-          </h5>
-        </Link>
-        </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
