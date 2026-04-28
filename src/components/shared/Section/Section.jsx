65ef7368ddbb76fdd41141fa8362375289a0e863
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-27 21:59:28 -0300

    component Sectio document.title dinamico

--- src/components/shared/Section/Section.jsx
+++ src/components/shared/Section/Section.jsx
@@ -1,16 +1,21 @@
+import { useEffect } from "react";
 import "./Section.scss";
 
 export default function Section({title,items = [], renderItem}) {
+  useEffect(() => {
+    document.title = `Connect | ${title}`;
+  });
+
   return (
-    <section className="section__wrapper">
-      <div className="section__container">
-        <h3 className="section__title">
-          {title}
-        </h3>
-        <div className="section__card--list">
-          {items.map((item, index) => renderItem(item, index))}
+      <section className="section__wrapper">
+        <div className="section__container">
+          <h3 className="section__title">
+            {title}
+          </h3>
+          <div className="section__card--list">
+            {items.map((item, index) => renderItem(item, index))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
   )
 }
