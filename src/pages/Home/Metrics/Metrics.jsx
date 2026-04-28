4caa822103028ff1f94913238e69d75ea3d53ebc
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-04-27 23:27:09 -0300

    component Metrics refatorado

--- src/pages/Home/Metrics/Metrics.jsx
+++ src/pages/Home/Metrics/Metrics.jsx
@@ -1,32 +1,39 @@
-import "./Metrics.scss"
+import "./Metrics.scss";
+
+const metrics = [
+  {
+    id: 1,
+    value: "500+",
+    label: "Empresas Voluntárias"
+  },
+  {
+    id: 2,
+    value: "1.2K+",
+    label: "Projetos Realizados"
+  },
+  {
+    id: 3,
+    value: "50K+",
+    label: "Vidas Impactadas"
+  },
+];
 
 export default function Metrics() {
   return (
     <>
-      <div className="home__metrics--item">
-        <p className="home__metrics--title">
-          500+
-        </p>
-        <p className="home__metrics--text">
-          Empresas Voluntárias
-        </p>
-      </div>
-      <div className="home__metrics--item">
-        <p className="home__metrics--title">
-          1.2K+
-        </p>
-        <p className="home__metrics--text">
-          Projetos Realizados
-        </p>
-      </div>
-      <div className="home__metrics--item">
-        <p className="home__metrics--title">
-          50K+
-        </p>
-        <p className="home__metrics--text">
-          Vidas Impactadas
-        </p>
-      </div>
+      {metrics.map((metric) => (
+        <div
+          key={metric.id}
+          className="home__metrics--item"
+        >
+          <p className="home__metrics--title">
+            {metric.value}
+          </p>
+          <p className="home__metrics--text">
+            {metric.label}
+          </p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
