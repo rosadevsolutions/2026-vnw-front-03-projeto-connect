4c5a16453a9988f46107ceda7dad1d8ede260f03
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-05-04 16:37:24 -0300

    App criacaoo app___wrapper pra usar grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Footer from './components/shared/Footer/Footer'
 import Main from './components/shared/Main/Main'
 import Header from './components/shared/Header/Header'
+import './App.scss';
 
 export default function App() {
   return (
-    <>
+    <div className="app__wrapper" >
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   )
 }
